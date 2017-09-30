d31c23755249d8c371a092972fad36a840a58a0b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-30 03:37:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_makeup = @word.split("").sort
+    anagrams = word_array.collect do |possible_anagrams|
+      possible_anagrams_makeup = possible_anagrams.split("").sort
+      if possible_anagrams_makeup == word_makeup
+        possible_anagrams
+      end
+    end
+    anagrams.compact
+  end
+end
