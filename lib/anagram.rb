7b96eb35d5c82977fa4f5ee4ae23bce96c01dda7
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-30 03:32:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,9 @@ attr_accessor :word
   end
 
   def match(word_array)
-    word_makeup = @word.split
+    word_makeup = @word.split.sort
     word_array.collect do |possible_anagrams|
       possible_anagrams_makeup = possible_anagrams.split
+      if possible
   end
 end
